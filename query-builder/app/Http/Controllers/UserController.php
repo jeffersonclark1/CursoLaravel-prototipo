be416e0b0d3cce7dc4d35e7bfb7a74221a7137fb
Jefferson Clark <jeffersonclarkti@gmail.com>
2020-11-16 16:10:03 -0300

    ParseString

--- query-builder/app/Http/Controllers/UserController.php
+++ query-builder/app/Http/Controllers/UserController.php
@@ -31,13 +31,23 @@ class UserController extends Controller
 //            echo "#{$user->id} Nome : {$user->name} Status : {$user->status} <br>";
 //        }
 
-        DB::table('users')->where('id','<','500')->chunkById(10, function ($users){
-            foreach ($users as $user) {
-                echo "#{$user->id} Nome : {$user->name} Status : {$user->status} <br>";
-            }
-            echo "Encerrou um ciclo<br>";
+//        DB::table('users')->where('id','<','500')->chunkById(10, function ($users){
+//            foreach ($users as $user) {
+//                echo "#{$user->id} Nome : {$user->name} Status : {$user->status} <br>";
+//            }
+//            echo "Encerrou um ciclo<br>";
 //            sleep(1);
-        });
+//        });
+
+        $users = DB::table('users')
+//            ->whereIn('users.status',[0,1])
+            ->whereNotIn('users.status',[0,1])
+            ->whereNotNull('users.name')
+            ->get();
+
+        foreach ($users as $user) {
+            echo "#{$user->id} Nome : {$user->name} Status : {$user->status} <br>";
+        }
 
     }
 }
