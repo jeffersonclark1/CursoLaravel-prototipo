a51c3ba6eb0ddb7f2649ad99117908e8124e1fb2
Jefferson Clark <jeffersonclarkti@gmail.com>
2020-11-16 15:41:43 -0300

    Chunck, chunckbyid

--- query-builder/app/Http/Controllers/UserController.php
+++ query-builder/app/Http/Controllers/UserController.php
@@ -9,16 +9,35 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = DB::table('users')
-            ->select()
-            ->where()
-            ->orderBy()
-            ->limit()
-            ->offset()
-            ->get();
+//        $users = DB::table('users')
+//            ->select()
+//            ->where()
+//            ->orderBy()
+//            ->limit()
+//            ->offset()
+//            ->get();
+//
+//        foreach ($users as $user) {
+//            echo "#{$user->id} Nome : {$user->name}<br>";
+//        }
 
-        foreach ($users as $user) {
-            echo "#{$user->id} Nome : {$user->name}<br>";
-        }
+//        $users = DB::table('users')
+//            ->selectRaw('users.id, users.name, CASE WHEN users.status = 1 THEN "ATIVO" ELSE "INATIVO" END status ')
+//            ->whereRaw('(SELECT COUNT(1) from addresses a where a.user = users.id) > 2')
+//            ->whereRaw('users.status = 1')
+//            ->get();
+//
+//        foreach ($users as $user) {
+//            echo "#{$user->id} Nome : {$user->name} Status : {$user->status} <br>";
+//        }
+
+        DB::table('users')->where('id','<','500')->chunkById(10, function ($users){
+            foreach ($users as $user) {
+                echo "#{$user->id} Nome : {$user->name} Status : {$user->status} <br>";
+            }
+            echo "Encerrou um ciclo<br>";
+//            sleep(1);
+        });
+
     }
 }
