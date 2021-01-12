8e693498e225bac79fd571692d27bdd6720a0173
Jefferson Clark <jeffersonclarkti@gmail.com>
2021-01-12 09:00:09 -0300

    Routes

--- servicos-essenciais/routes/web.php
+++ servicos-essenciais/routes/web.php
@@ -20,3 +20,32 @@ Route::get('/', function () {
 Route::get('/log',function(){
    Log::info('teste');
 });
+
+Route::get('/session',function(){
+
+    session([
+        'course'=> 'LaraDev'
+    ]);
+
+    session()->put('name', 'Jefferson Clark');
+
+//    echo session('name','Valor Padrao');
+
+//    echo session()->get('name');
+
+//    session()->push('time','Hannah Clark');
+
+//    $time = session()->pull('time');
+//
+//    echo $time;
+
+//    session()->forget('name');
+
+//    session()->flush();
+
+    if(session()->has('course')){
+        echo "o curso selecionado foi : " . session()->get('course') . "<br>";
+    }
+
+    var_dump(\session()->all());
+});
